70f46fe75267f836d66d7255319594cad42ff31d
Mr zh <877854886@qq.com>
2019-01-25 10:14:28 +0800

    test

--- pages/userStoreNotice/userStoreNotice.js
+++ pages/userStoreNotice/userStoreNotice.js
@@ -18,8 +18,46 @@ Page({
 
   onLoad(){
     const self = this;
-    
+   api.commonInit(self);
+    self.getMainData()
   },
+
+  getMainData(){
+    const self = this;
+    const postData = {};
+    postData.searchItem = {
+      thirdapp_id:getApp().globalData.thirdapp_id
+    };
+    postData.getBefore = {
+      article:{
+        tableName:'Label',
+        searchItem:{
+          title:['=',['开店规则']],
+          thirdapp_id:['=',[getApp().globalData.thirdapp_id]],
+        },
+        middleKey:'menu_id',
+        key:'id',
+        condition:'in',
+      },
+    };
+    const callback = (res)=>{
+        if(res.info.data.length>0){
+          self.data.mainData=res.info.data[0]
+          self.data.mainData.content = api.wxParseReturn(res.info.data[0].content).nodes;
+        }
+        
+        
+        api.checkLoadAll(self.data.isFirstLoadAllStandard,'getMainData',self);
+      
+      self.setData({
+        web_mainData:self.data.mainData,
+      });  
+    };
+    api.articleGet(postData,callback);
+  },
+
+
+
 
   onShow(){
     const self = this;
